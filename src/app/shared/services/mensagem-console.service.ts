fc5ca50559a6d54facb78578fd8c0fc45262b127
Ian-Ribeiro <ian.ribeiro@academico.ifpb.edu.br>
2023-11-20 10:38:57 -0300

    Initial commit

---
+++ src/app/shared/services/mensagem-console.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {IMensagem} from "../modelo/IMensagem";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MensagemConsoleService extends IMensagem {
+
+  constructor() {
+    super();
+  }
+
+  erro(mensagem: string) {
+    console.log(mensagem);
+  }
+
+  info(mensagem: string): void {
+    console.log(mensagem);
+  }
+
+  sucesso(mensagem: string): void {
+    console.log(mensagem);
+  }
+
+}
